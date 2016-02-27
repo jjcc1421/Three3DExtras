13e10a82538503a6b8d8aedd8cce9c863a0f26a9
Juan Caicedo <jjcc1421@gmail.com>
2016-02-27 12:56:46 -0500

    added tube

--- app/tubeLine.js
+++ app/tubeLine.js
@@ -6,35 +6,36 @@
  * @paran color #hexColor default '#000'
  */
 if (three3DExtras) {
-    if (three3DExtras.tubeLine) {
-        three3DExtras.prototype.tubeLine = function (origin, destination, weight, color) {
+    if (!three3DExtras.tubeLine) {
+        three3DExtras.tubeLine = function (origin, destination, weight, color) {
             weight = weight ? 1 : weight;
-            color = color ? "#000" : color;
+            color = color ? color : "#000";
             this.origin = origin;
             this.destination = destination;
             this.weight = weight;
             this.color = color;
-            this.getObject3D = function () {
-                var line = new THREE.CatmullRomCurve3([
-                    new THREE.Vector3(origin[0], origin[1], origin[2]),
-                    new THREE.Vector3(destination[0], destination[1], destination[2]),
-                ]);
-                var geometryTube = new THREE.TubeGeometry(
-                    line,  //path
-                    1,    //segments
-                    weight,     //radius
-                    8,     //radiusSegments
-                    false  //closed
-                    );
-                var material = new THREE.MeshBasicMaterial({
-                    color: color,
-                    wireframe: false,
-                    transparent: true,
-                    //opacity:0.5
-                });
-                var object3d = new THREE.Mesh(geometryTube, material);
-                return object3d;
-            }
+
+        }
+        three3DExtras.tubeLine.prototype.getObject3D = function () {
+            var line = new THREE.CatmullRomCurve3([
+                new THREE.Vector3(this.origin[0], this.origin[1], this.origin[2]),
+                new THREE.Vector3(this.destination[0], this.destination[1], this.destination[2]),
+            ]);
+            var geometryTube = new THREE.TubeGeometry(
+                line,  //path
+                1,    //segments
+                this.weight,     //radius
+                8,     //radiusSegments
+                true  //closed
+                );
+            var material = new THREE.MeshBasicMaterial({
+                color: this.color,
+                wireframe: false,
+                transparent: true,
+                //opacity:0.5
+            });
+            var object3d = new THREE.Mesh(geometryTube, material);
+            return object3d;
         }
     }
 }
